09d3a4aea0f612ea1253cfb54e2ee7a1dee6e58f
Jacob Graham <jacobgraham2025@u.northwestern.edu>
2025-04-02 16:03:56 -0500

    Working equilibration and smd protocols with incorrect periodic boundary conditions. Images are too close together.

--- make_bis_catechol_iron_crosslink_psf.tcl
+++ make_bis_catechol_iron_crosslink_psf.tcl
@@ -8,11 +8,17 @@ topology charmm2lammps_all36_tip3/top_all36_prot_C2L.rtf
 # Add the first divalent dopa.
 segment P1 { pdb do2p_1.pdb }
 coordpdb do2p_1.pdb P1
+# For some reason the commands below only neutralized the carboxyl group and not the amine.
+# patch CNEU P1:28
+# patch NNEU P1:28
 guesscoord
 
 # Add the second divalent dopa.
 segment P2 { pdb do2p_2.pdb }
 coordpdb do2p_2.pdb P2
+# For some reason the commands below only neutralized the carboxyl group and not the amine.
+# patch CNEU P2:67
+# patch NNEU P2:67
 guesscoord
 
 # Add the iron atom.
---
+++ smd_simulation/submit_smd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name="smd_bis-catechol_complex"
+#SBATCH -A p31412
+#SBATCH -p short   ## partition
+#SBATCH -n 4  ## number of cores
+#SBATCH --output=R-%x.%j.out
+#SBATCH --constraint="[quest10|quest11|quest12]"
+#SBATCH -t 00:10:00
+
+## cd $PBS_O_WORKDIR
+module purge all
+module load lammps/20200303-openmpi-4.0.5-intel-19.0.5.281
+
+mpirun -np 4 lmp -in smd.in -log smd.log
