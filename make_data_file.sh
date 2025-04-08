be7e7196fa01c938ed5393789e6c0c3bba2e0f15
Jacob Graham <jacobgraham2025@u.northwestern.edu>
2025-04-08 14:18:46 -0500

    Fully automated the simulation generation protocol by running make_data_file.sh

--- make_data_file.sh
+++ make_data_file.sh
@@ -16,3 +16,22 @@ fi
 
 # Run a bash script to create a LAMMPS data file from the PSF.
 bash run_c2l.sh > run_c2l.log
+
+# Organize log and output files into a simulation directory.
+cp -r smd_simulation_template smd_simulation
+
+# Make a directory to store log files generated during the simulation creation.
+mkdir smd_simulation/temp_files
+
+# Move pdb, psf, and data files into the smd_simulation directory.
+cp charmm2lammps_all36_tip3/bis_catechol_iron_complex.p* charmm2lammps_all36_tip3/bis_catechol_iron_complex.data smd_simulation
+
+# Move llog files into teh temp_files directory.
+mv run_c2l.log solvate_ionize.log make_bis_catechol_iron_crosslink_psf.log smd_simulation/temp_files
+
+# Make a copy of the charm2lammps_all36_tip3 diretory in the newly created smd_simulation folder.
+cp -r charmm2lammps_all36_tip3 smd_simulation/
+
+# Clean up the files in the charmm2lammps_all36_tip3 folder.
+cd charmm2lammps_all36_tip3
+rm bis_catechol_iron* charmm2lammps.log
