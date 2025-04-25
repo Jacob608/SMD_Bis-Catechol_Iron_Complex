4cb86c2d0e611865c8f6b22bea716e506c321a31
Jacob Graham <jacobgraham2025@u.northwestern.edu>
2025-04-25 11:25:47 -0500

    Added -tether_dist to arguments for calculate_xyz_for_fix_smd.py. Added inspect_structure.vmd'

--- smd_simulation_template/calculate_xyz_for_fix_smd.py
+++ smd_simulation_template/calculate_xyz_for_fix_smd.py
@@ -15,12 +15,15 @@ parser.add_argument('-idx1', type=int, help='First index')
 # idx2 is the index of the atom which will be attached to the spring during the smd simulation.
 parser.add_argument('-idx2', type=int, help='Second index')
 # f is the name of the data file from which the positions of id1 and id2 will be extracted.
-parser.add_argument('-f', type=str, help='A string argument')
+parser.add_argument('-f', type=str, help='LAMMPS data file name.')
+# tether_dist is the initial distance between the pull atom and the other end of the smd spring.
+parser.add_argument('-tether_dist', type=str, help='Distance between pull atom and other end of smd spring.')
 
 args = parser.parse_args()
 idx1 = args.idx1
 idx2 = args.idx2
 f = args.f
+tether_dist = args.tether_dist
 
 def unit_vector(pos1, pos2):
     """
@@ -42,7 +45,7 @@ def unit_vector(pos1, pos2):
     # Convert the difference between pos2 and pos1 into a unit vector.
     unit_vec = np.asarray([delta_x, delta_y, delta_z]/magnitude)
     
-    # Make sure that this is a unit vector.
+    # Make sure that this is a unit vectorto two decimal places.
     assert np.round(np.sqrt(unit_vec[0]**2 + unit_vec[1]**2 + unit_vec[2]**2), 2) == 1.0
     
     return unit_vec
@@ -56,7 +59,7 @@ pos2 = u.select_atoms(f"index {idx2}").positions[0]
 unit_vec = unit_vector(pos1, pos2)
 # Print the x, y, and z coordinates of the unit vector to a text file.
 with open('xyz.txt', 'w') as file:
-    file.write(f"{pos2[0] + 10 * unit_vec[0]} {pos2[1] + 10 * unit_vec[1]} {pos2[2] + 10 * unit_vec[2]}")
+    file.write(f"{pos2[0] + tether_dist * unit_vec[0]} {pos2[1] + tether_dist * unit_vec[1]} {pos2[2] + tether_dist * unit_vec[2]}")
 
 
 
